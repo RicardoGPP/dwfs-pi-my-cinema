19701a12e27dbc872136fcb839693c4b11a351e2
rperandre <rperandre@padtec.com.br>
2024-10-11 21:56:41 -0300

    Adding some more comment endpoints.

--- backend/src/main/java/br/pucminas/dwfs/pi/core/comment/boundary/resource/CommentResource.java
+++ backend/src/main/java/br/pucminas/dwfs/pi/core/comment/boundary/resource/CommentResource.java
@@ -1,20 +1,28 @@
 package br.pucminas.dwfs.pi.core.comment.boundary.resource;
 
+import java.util.List;
+
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
 import br.pucminas.dwfs.pi.core.comment.control.service.CommentService;
 import br.pucminas.dwfs.pi.core.comment.entity.Comment;
 import br.pucminas.dwfs.pi.core.movie.control.service.MovieService;
+import br.pucminas.dwfs.pi.core.movie.entity.Movie;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/comments")
+@Tag(name = "Comments", description = "Resource for interacting with movie comments.")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class CommentResource {
@@ -24,6 +32,41 @@ public class CommentResource {
 
     @Inject
     MovieService movieService;
+
+    @GET
+    public Response getAllByMovie(@QueryParam("movie-id") Long movieId) {
+        Movie movie = movieService.getById(movieId);
+
+        if (movie == null) {
+            return Response
+                .status(Response.Status.NOT_FOUND)
+                .build();
+        }
+
+        return Response
+            .ok()
+            .entity(commentService.getAllByMovie(movie))
+            .build();
+    }
+
+    @GET
+    @Path("/summary")
+    public Response getSummary(@QueryParam("movie-id") Long movieId) {
+        Movie movie = movieService.getById(movieId);
+
+        if (movie == null) {
+            return Response
+                .status(Response.Status.NOT_FOUND)
+                .build();
+        }
+
+        List<Comment> comments = commentService.getAllByMovie(movie);
+
+        return Response
+            .ok()
+            .entity(commentService.getSummary(comments))
+            .build();
+    }
 
     @POST
     @RolesAllowed({"USER", "ADMIN"})
