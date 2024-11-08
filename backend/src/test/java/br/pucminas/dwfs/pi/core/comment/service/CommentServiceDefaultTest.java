448d960a30bdc11521db8daa057ddf572c0db6e7
rperandre <rperandre@padtec.com.br>
2024-11-08 20:43:27 -0300

    Small comment adjustment.

--- backend/src/test/java/br/pucminas/dwfs/pi/core/comment/service/CommentServiceDefaultTest.java
+++ backend/src/test/java/br/pucminas/dwfs/pi/core/comment/service/CommentServiceDefaultTest.java
@@ -33,7 +33,7 @@ import jakarta.transaction.Transactional;
 /**
  * Tests the comment service default class.
  * 
- * @author Ricardo Giovani Piantavinha Perandré (rperandre)
+ * @author Ricardo Giovani Piantavinha Perandré (RicardoGPP)
  * @version 1.0
  * @since 08/11/2024
  */
